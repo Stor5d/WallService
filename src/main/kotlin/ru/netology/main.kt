79552572d1a045a6271234f4bb93f8ec79149f3d
stor5d <stor5d@mail.ru>
2022-03-30 01:13:33 +0300

    minor edits

--- src/main/kotlin/ru/netology/main.kt
+++ src/main/kotlin/ru/netology/main.kt
@@ -3,33 +3,48 @@ package ru.netology
 import java.util.*
 
 fun main() {
-    val comments = Comments(1, canPost = true, groupsCanPost = true, canClose = true, canOpen = true)
-    val copyright = Copyright(1, "www.yandex.ru", "яндекс", "all")
-    val likes = Likes(111, userLikes = true, canLike = true, canPublish = true)
-    val reposts = Reposts(111, true)
-    val views = Views(123)
-    val donut = Donut(true, 1234, true, "all")
     val post1 = Post(
-        0, 1, 2, 3, Date(), "текст поста 1", 4, 5, true,
-        comments, copyright, likes, reposts, views, "post", null, null, 1, canPin = true, canDelete = true,
-        canEdit = true, isPinned = true, markedAsAds = true, isFavorite = true, donut = donut, postponedId = 1223
+        0,
+        1,
+        2,
+        3,
+        Date(),
+        "текст поста 1",
+        4,
+        5,
+        true,
+        Comments(1, canPost = true, groupsCanPost = true, canClose = true, canOpen = true),
+        Copyright(1, "www.yandex.ru", "яндекс", "all"),
+        Likes(111, userLikes = true, canLike = true, canPublish = true),
+        Reposts(111, true),
+        Views(123),
+        "post",
+        null,
+        null,
+        1,
+        canPin = true,
+        canDelete = true,
+        canEdit = true,
+        isPinned = true,
+        markedAsAds = true,
+        isFavorite = true,
+        donut = Donut(true, 1234, true, "all"),
+        postponedId = 1223
     )
     val audio = Audio(1, 2, "Валерий Леонтьев", "Художник", 223, "www.musik.ru/leo/hudozhnik")
     val attachment1 = AudioAttachment(audio)
-    post1.addAttachments(attachment1)
-    val video = Video(
-        333, 444, "Летящая обезьяна", "обезьяна летит по горам",
-        40, 11212121, 13223232, 30, "www.youtube.com"
+    post1.addAttachments(
+        AudioAttachment(Audio(1, 2, "Валерий Леонтьев", "Художник", 223, "www.musik.ru/leo/hudozhnik"))
     )
-    val attachment2 = VideoAttachment(video)
-    post1.addAttachments(attachment2)
-    val post2 = Post(
-        0, 1, 2, 3, Date(), "текст поста 2", 4, 5, true,
-        comments, copyright, likes, reposts, views, "post", null, null, 1, canPin = true, canDelete = true,
-        canEdit = true, isPinned = true, markedAsAds = true, isFavorite = true, donut = donut, postponedId = 1223
+    post1.addAttachments(
+        VideoAttachment(
+            Video(
+                333, 444, "Летящая обезьяна", "обезьяна летит по горам", 40,
+                11212121, 13223232, 30, "www.youtube.com"
+            )
+        )
     )
     WallService.add(post1)
-    WallService.add(post2)
 
 }
 
