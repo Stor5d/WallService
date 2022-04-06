81ec2fe08c47a10f1e435f36622e72a1dfa4342b
stor5d <stor5d@mail.ru>
2022-04-06 23:42:21 +0300

    work on bugs 1

--- src/main/kotlin/ru/netology/main.kt
+++ src/main/kotlin/ru/netology/main.kt
@@ -1,221 +1,36 @@
 package ru.netology
 
+import ru.netology.Attachments.Audio
+import ru.netology.Attachments.AudioAttachment
+import ru.netology.Attachments.Video
+import ru.netology.Attachments.VideoAttachment
 import java.util.*
 
 fun main() {
-    val post1 = Post(
-        0,
-        1,
-        2,
-        3,
-        Date(),
-        "текст поста 1",
-        4,
-        5,
-        true,
-        Comments(1, canPost = true, groupsCanPost = true, canClose = true, canOpen = true),
-        Copyright(1, "www.yandex.ru", "яндекс", "all"),
-        Likes(111, userLikes = true, canLike = true, canPublish = true),
-        Reposts(111, true),
-        Views(123),
-        "post",
-        null,
-        null,
-        1,
-        canPin = true,
-        canDelete = true,
-        canEdit = true,
-        isPinned = true,
-        markedAsAds = true,
-        isFavorite = true,
-        donut = Donut(true, 1234, true, "all"),
-        postponedId = 1223
+    val thread = Thread(999, canPost = true, showRetlyButton = true, groupsCanPost = true)
+    val comments = Comments(1, canPost = true, groupsCanPost = true, canClose = true, canOpen = true)
+    val post = Post(
+        0, 1, 2, 3, Date(), "здесь тектс поста", 4, 5,
+        true, comments, null, null, null, null, "post", null,
+        null, 7676, copyHistory = null, canPin = true, canDelete = true, canEdit = true, isPinned = true,
+        markedAsAds = true, isFavorite = true, donut = null, postponedId = 2323
     )
     val audio = Audio(1, 2, "Валерий Леонтьев", "Художник", 223, "www.musik.ru/leo/hudozhnik")
-    val attachment1 = AudioAttachment(audio)
-    post1.addAttachments(
-        AudioAttachment(Audio(1, 2, "Валерий Леонтьев", "Художник", 223, "www.musik.ru/leo/hudozhnik"))
+    val attachmentAudio = AudioAttachment(audio)
+    post.addAttachments(attachmentAudio)
+    val video = Video(
+        333, 444, "Летящая обезьяна", "обезьяна летит по горам", 40,
+        11212121, 13223232, 30, "www.youtube.com"
     )
-    post1.addAttachments(
-        VideoAttachment(
-            Video(
-                333, 444, "Летящая обезьяна", "обезьяна летит по горам", 40,
-                11212121, 13223232, 30, "www.youtube.com"
-            )
-        )
+    val attachmentVideo = VideoAttachment(video)
+    post.addAttachments(attachmentVideo)
+    WallService.add(post)
+    val comment = Comment(
+        666, 1, 23000, 111, "тут текст комментария", null, null,
+        null, null, thread
     )
-    WallService.add(post1)
-
+    WallService.createComment(comment)
+    val reportComment = ReportComment(222, 666, 1)
+    WallService.createReportComment(reportComment)
 }
-
-object WallService {
-    private var posts = emptyArray<Post>()
-    private var nextId: Int = 1
-
-    fun add(post: Post): Post {
-        post.id = nextId
-        nextId++
-        posts += post
-        return posts.last()
-    }
-
-    fun update(post: Post): Boolean {
-        for ((index, postOrigin) in posts.withIndex()) {
-            if (post.id == postOrigin.id) {
-                posts[index] = post.copy(id = postOrigin.id, date = postOrigin.date)
-                return true
-            }
-        }
-        return false
-    }
-}
-
-data class Post(
-    var id: Int = 0,
-    val ownerId: Int,
-    val fromId: Int,
-    val createdBy: Int,
-    val date: Date = Date(),
-    val text: String,
-    val replyOwnerId: Int,
-    val replyPostId: Int,
-    val friendsOnly: Boolean,
-    val comments: Comments?,
-    val copyright: Copyright?,
-    val likes: Likes?,
-    val reposts: Reposts?,
-    val views: Views?,
-    val postType: String,
-    val postSource: PostSource?,
-    val geo: Geo?,
-    val singerId: Int,
-    val copyHistory: Array<String> = emptyArray<String>(),
-    val canPin: Boolean,
-    val canDelete: Boolean,
-    val canEdit: Boolean,
-    val isPinned: Boolean,
-    val markedAsAds: Boolean,
-    val isFavorite: Boolean,
-    val donut: Donut?,
-    val postponedId: Int
-) {
-    private var attachments = emptyArray<Attachment>()
-
-    fun addAttachments(attachment: Attachment): Boolean {
-        attachments += attachment
-        return true
-    }
-}
-
-data class Comments(
-    val count: Int,
-    var canPost: Boolean,
-    var groupsCanPost: Boolean,
-    var canClose: Boolean,
-    var canOpen: Boolean
-)
-
-data class Copyright(
-    val id: Int,
-    var link: String,
-    var name: String,
-    var type: String
-)
-
-data class Likes(
-    var count: Int,
-    var userLikes: Boolean,
-    var canLike: Boolean,
-    var canPublish: Boolean
-)
-
-data class Reposts(
-    var count: Int,
-    var userReposted: Boolean
-)
-
-data class Views(val count: Int)
-
-data class PostSource(
-    val type: String,
-    val platform: String,
-    val data: String,
-    val url: String
-)
-
-data class Geo(
-    val type: String,
-    val coordinates: String,
-    val plase: Objects
-)
-
-data class Donut(
-    val isDonut: Boolean,
-    val paidDuration: Int,
-    val canPublishFreeCopy: Boolean,
-    val editMode: String
-)
-
-sealed class Attachment(val type: String)
-
-data class AudioAttachment(val audio: Audio) : Attachment("audio")
-
-class Audio(
-    val id: Int,
-    val ownerId: Int,
-    val artist: String,
-    val title: String,
-    val duration: Int,
-    val url: String
-)
-
-data class VideoAttachment(val video: Video) : Attachment("video")
-
-class Video(
-    val id: Int,
-    val ownerId: Int,
-    val title: String,
-    val description: String,
-    val duration: Int,
-    val date: Int,
-    val addingDate: Int,
-    val views: Int,
-    val url: String
-)
-
-data class PhotoAttachment(val photo: Photo) : Attachment("photo")
-
-class Photo(
-    val id: Int,
-    val albumId: Int,
-    val ownerId: Int,
-    val userId: Int,
-    val text: String,
-    val date: Int,
-    val width: Int,
-    val height: Int,
-    val url: String
-)
-
-data class FileAttachment(val file: File) : Attachment("file")
-
-class File(
-    val id: Int,
-    val ownerId: Int,
-    val title: String,
-    val ext: String,
-    val size: Int,
-    val date: Int,
-    val type: Int,
-    val url: String
-)
-
-data class LinkAttachment(val link: Link) : Attachment("link")
-
-class Link(
-    val url: String,
-    val title: String,
-    val caption: String,
-    val description: String,
-)
 
--- src/test/kotlin/ru/netology/WallServiceTest.kt
+++ src/test/kotlin/ru/netology/WallServiceTest.kt
@@ -1,14 +1,23 @@
 package ru.netology
 
+import org.junit.After
 import org.junit.Test
 import org.junit.Assert.*
+import ru.netology.Attachments.*
+import ru.netology.Exception.CommentNotFoundException
+import ru.netology.Exception.PostNotFoundException
+import ru.netology.Exception.WrongReasonException
 import java.util.*
 
 class WallServiceTest {
+    @After
+    fun clear() {
+        WallService.clearPosts()
+    }
 
     @Test
     fun add() {
-        val expectedId = 0
+        val expectedId = 1
         val comments = Comments(1, canPost = true, groupsCanPost = true, canClose = true, canOpen = true)
         val copyright = Copyright(1, "www.yandex.ru", "яндекс", "all")
         val likes = Likes(111, userLikes = true, canLike = true, canPublish = true)
@@ -17,7 +26,7 @@ class WallServiceTest {
         val donut = Donut(true, 1234, true, "all")
         val post1 = Post(
             0, 1, 2, 3, Date(), "текст поста 1", 4, 5, true,
-            comments, copyright, likes, reposts, views, "post", null, null, 1, canPin = true, canDelete = true,
+            comments, copyright, likes, reposts, views, "post", null, null, 1, null, canPin = true, canDelete = true,
             canEdit = true, isPinned = true, markedAsAds = true, isFavorite = true, donut = donut, postponedId = 1223
         )
         WallService.add(post1)
@@ -35,13 +44,13 @@ class WallServiceTest {
         val donut = Donut(true, 1234, true, "all")
         val post1 = Post(
             0, 1, 2, 3, Date(), "текст поста 1", 4, 5, true,
-            comments, copyright, likes, reposts, views, "post", null, null, 1, canPin = true, canDelete = true,
+            comments, copyright, likes, reposts, views, "post", null, null, 1, null, canPin = true, canDelete = true,
             canEdit = true, isPinned = true, markedAsAds = true, isFavorite = true, donut = donut, postponedId = 1223
         )
         WallService.add(post1)
         val post2 = Post(
             1, 1, 2, 3, Date(), "текст поста 2", 4, 5, true,
-            comments, copyright, likes, reposts, views, "post", null, null, 1, canPin = true, canDelete = true,
+            comments, copyright, likes, reposts, views, "post", null, null, 1, null, canPin = true, canDelete = true,
             canEdit = true, isPinned = true, markedAsAds = true, isFavorite = true, donut = donut, postponedId = 1223
         )
         val actualUpdate = WallService.update(post2)
@@ -58,13 +67,13 @@ class WallServiceTest {
         val donut = Donut(true, 1234, true, "all")
         val post1 = Post(
             0, 1, 2, 3, Date(), "текст поста 1", 4, 5, true,
-            comments, copyright, likes, reposts, views, "post", null, null, 1, canPin = true, canDelete = true,
+            comments, copyright, likes, reposts, views, "post", null, null, 1, null, canPin = true, canDelete = true,
             canEdit = true, isPinned = true, markedAsAds = true, isFavorite = true, donut = donut, postponedId = 1223
         )
         WallService.add(post1)
         val post2 = Post(
             999, 1, 2, 3, Date(), "текст поста 2", 4, 5, true,
-            comments, copyright, likes, reposts, views, "post", null, null, 1, canPin = true, canDelete = true,
+            comments, copyright, likes, reposts, views, "post", null, null, 1, null, canPin = true, canDelete = true,
             canEdit = true, isPinned = true, markedAsAds = true, isFavorite = true, donut = donut, postponedId = 1223
         )
         val actualUpdate = WallService.update(post2)
@@ -81,10 +90,10 @@ class WallServiceTest {
         val donut = Donut(true, 1234, true, "all")
         val post = Post(
             0, 1, 2, 3, Date(), "текст поста 1", 4, 5, true,
-            comments, copyright, likes, reposts, views, "post", null, null, 1, canPin = true, canDelete = true,
+            comments, copyright, likes, reposts, views, "post", null, null, 1, null, canPin = true, canDelete = true,
             canEdit = true, isPinned = true, markedAsAds = true, isFavorite = true, donut = donut, postponedId = 1223
         )
-        val audio = Audio(1, 2, "Валерий Леонтьев", "Художник", 223, "www.musik.ru/leo/hudozhnik")
+        val audio = Audio(1, 2, "Валерий Леонтьев", "Художник", 223, "www.musk.ru/leo")
         val attachment = AudioAttachment(audio)
         WallService.add(post)
         assertTrue(post.addAttachments(attachment))
@@ -100,7 +109,7 @@ class WallServiceTest {
         val donut = Donut(true, 1234, true, "all")
         val post = Post(
             0, 1, 2, 3, Date(), "текст поста 1", 4, 5, true,
-            comments, copyright, likes, reposts, views, "post", null, null, 1, canPin = true, canDelete = true,
+            comments, copyright, likes, reposts, views, "post", null, null, 1, null, canPin = true, canDelete = true,
             canEdit = true, isPinned = true, markedAsAds = true, isFavorite = true, donut = donut, postponedId = 1223
         )
         val video = Video(
@@ -122,7 +131,7 @@ class WallServiceTest {
         val donut = Donut(true, 1234, true, "all")
         val post = Post(
             0, 1, 2, 3, Date(), "текст поста 1", 4, 5, true,
-            comments, copyright, likes, reposts, views, "post", null, null, 1, canPin = true, canDelete = true,
+            comments, copyright, likes, reposts, views, "post", null, null, 1, null, canPin = true, canDelete = true,
             canEdit = true, isPinned = true, markedAsAds = true, isFavorite = true, donut = donut, postponedId = 1223
         )
         val photo = Photo(1, 1, 1, 1, "собака", 1, 300, 250, "www.photo.ru")
@@ -141,7 +150,7 @@ class WallServiceTest {
         val donut = Donut(true, 1234, true, "all")
         val post = Post(
             0, 1, 2, 3, Date(), "текст поста 1", 4, 5, true,
-            comments, copyright, likes, reposts, views, "post", null, null, 1, canPin = true, canDelete = true,
+            comments, copyright, likes, reposts, views, "post", null, null, 1, null, canPin = true, canDelete = true,
             canEdit = true, isPinned = true, markedAsAds = true, isFavorite = true, donut = donut, postponedId = 1223
         )
         val file = File(1, 1, "ds", "txt", 100, 1, 1, "www.files.ru")
@@ -160,7 +169,7 @@ class WallServiceTest {
         val donut = Donut(true, 1234, true, "all")
         val post = Post(
             0, 1, 2, 3, Date(), "текст поста 1", 4, 5, true,
-            comments, copyright, likes, reposts, views, "post", null, null, 1, canPin = true, canDelete = true,
+            comments, copyright, likes, reposts, views, "post", null, null, 1, null, canPin = true, canDelete = true,
             canEdit = true, isPinned = true, markedAsAds = true, isFavorite = true, donut = donut, postponedId = 1223
         )
         val link = Link("www.links.com", "заголовок", "заголовок", "ссылка на заголовок")
@@ -179,7 +188,7 @@ class WallServiceTest {
         val donut = Donut(true, 1234, true, "all")
         val post = Post(
             0, 1, 2, 3, Date(), "текст поста 1", 4, 5, true,
-            comments, copyright, likes, reposts, views, "post", null, null, 1, canPin = true, canDelete = true,
+            comments, copyright, likes, reposts, views, "post", null, null, 1, null, canPin = true, canDelete = true,
             canEdit = true, isPinned = true, markedAsAds = true, isFavorite = true, donut = donut, postponedId = 1223
         )
         val expectedId = 0
@@ -196,11 +205,191 @@ class WallServiceTest {
         val donut = Donut(true, 1234, true, "all")
         val post = Post(
             0, 1, 2, 3, Date(), "текст поста 1", 4, 5, true,
-            comments, copyright, likes, reposts, views, "post", null, null, 1, canPin = true, canDelete = true,
+            comments, copyright, likes, reposts, views, "post", null, null, 1, null, canPin = true, canDelete = true,
             canEdit = true, isPinned = true, markedAsAds = true, isFavorite = true, donut = donut, postponedId = 1223
         )
         val expectedText = "текст поста 1"
         assertEquals(post.text, expectedText)
+    }
+
+    @Test(expected = PostNotFoundException::class)
+    fun createCommentPostNotFoundException() {
+        val comments = Comments(1, canPost = true, groupsCanPost = true, canClose = true, canOpen = true)
+        val post1 = Post(
+            0, 1, 2, 3, Date(), "здесь тектс поста", 4, 5,
+            true, comments, null, null, null, null, "post", null,
+            null, 7676, copyHistory = null, true, true, true, true,
+            true, true, null, 2323
+        )
+        val post2 = Post(
+            0, 1, 2, 3, Date(), "здесь тектс поста", 4, 5,
+            true, comments, null, null, null, null, "post", null,
+            null, 7676, copyHistory = null, true, true, true, true,
+            true, true, null, 2323
+        )
+        WallService.add(post1)
+        WallService.add(post2)
+        val thread = Thread(999, true, true, true)
+        val comment = Comment(
+            666, 3, 23000, 111, "тут текст комментария", null, null,
+            null, null, thread
+        )
+        WallService.createComment(comment)
+    }
+
+    @Test
+    fun createCommentTrue() {
+        val comments = Comments(1, canPost = true, groupsCanPost = true, canClose = true, canOpen = true)
+        val post1 = Post(
+            0, 1, 2, 3, Date(), "здесь тектс поста", 4, 5,
+            true, comments, null, null, null, null, "post", null,
+            null, 7676, copyHistory = null, true, true, true, true,
+            true, true, null, 2323
+        )
+        val post2 = Post(
+            0, 1, 2, 3, Date(), "здесь тектс поста", 4, 5,
+            true, comments, null, null, null, null, "post", null,
+            null, 7676, copyHistory = null, true, true, true, true,
+            true, true, null, 2323
+        )
+        WallService.add(post1)
+        WallService.add(post2)
+        val thread = Thread(999, true, true, true)
+        val comment = Comment(
+            666, 2, 23000, 111, "тут текст комментария", null, null,
+            null, null, thread
+        )
+        assert(WallService.createComment(comment))
+    }
+
+    @Test
+    fun createReportCommentTrue() {
+        val comments = Comments(1, canPost = true, groupsCanPost = true, canClose = true, canOpen = true)
+        val post1 = Post(
+            0, 1, 2, 3, Date(), "здесь тектс поста", 4, 5,
+            true, comments, null, null, null, null, "post", null,
+            null, 7676, copyHistory = null, true, true, true, true,
+            true, true, null, 2323
+        )
+        val post2 = Post(
+            0, 1, 2, 3, Date(), "здесь тектс поста", 4, 5,
+            true, comments, null, null, null, null, "post", null,
+            null, 7676, copyHistory = null, true, true, true, true,
+            true, true, null, 2323
+        )
+        WallService.add(post1)
+        WallService.add(post2)
+        val thread = Thread(999, true, true, true)
+        val comment = Comment(
+            666, 2, 23000, 111, "тут текст комментария", null, null,
+            null, null, thread
+        )
+        WallService.createComment(comment)
+        val reportComment = ReportComment(222, 666, 2)
+        assert(WallService.createReportComment(reportComment))
+    }
+
+    @Test(expected = CommentNotFoundException::class)
+    fun createReportCommentNotFoundComment() {
+        val comments = Comments(1, canPost = true, groupsCanPost = true, canClose = true, canOpen = true)
+        val post1 = Post(
+            0, 1, 2, 3, Date(), "здесь тектс поста", 4, 5,
+            true, comments, null, null, null, null, "post", null,
+            null, 7676, copyHistory = null, true, true, true, true,
+            true, true, null, 2323
+        )
+        val post2 = Post(
+            0, 1, 2, 3, Date(), "здесь тектс поста", 4, 5,
+            true, comments, null, null, null, null, "post", null,
+            null, 7676, copyHistory = null, true, true, true, true,
+            true, true, null, 2323
+        )
+        WallService.add(post1)
+        WallService.add(post2)
+        val thread = Thread(999, true, true, true)
+        val comment = Comment(
+            666, 2, 23000, 111, "тут текст комментария", null, null,
+            null, null, thread
+        )
+        WallService.createComment(comment)
+        val reportComment = ReportComment(222, 777, 2)
+        WallService.createReportComment(reportComment)
+    }
+
+    @Test(expected = WrongReasonException::class)
+    fun createReportCommentWrongReasonExceptionLess() {
+        val comments = Comments(1, canPost = true, groupsCanPost = true, canClose = true, canOpen = true)
+        val post1 = Post(
+            0, 1, 2, 3, Date(), "здесь тектс поста", 4, 5,
+            true, comments, null, null, null, null, "post", null,
+            null, 7676, copyHistory = null, true, true, true, true,
+            true, true, null, 2323
+        )
+        val post2 = Post(
+            0, 1, 2, 3, Date(), "здесь тектс поста", 4, 5,
+            true, comments, null, null, null, null, "post", null,
+            null, 7676, copyHistory = null, true, true, true, true,
+            true, true, null, 2323
+        )
+        WallService.add(post1)
+        WallService.add(post2)
+        val thread = Thread(999, true, true, true)
+        val comment = Comment(
+            666, 2, 23000, 111, "тут текст комментария", null, null,
+            null, null, thread
+        )
+        WallService.createComment(comment)
+        val reportComment = ReportComment(222, 666, -1)
+        WallService.createReportComment(reportComment)
+    }
+
+    @Test(expected = WrongReasonException::class)
+    fun createReportCommentWrongReasonExceptionMore() {
+        val comments = Comments(1, canPost = true, groupsCanPost = true, canClose = true, canOpen = true)
+        val post1 = Post(
+            0, 1, 2, 3, Date(), "здесь тектс поста", 4, 5,
+            true, comments, null, null, null, null, "post", null,
+            null, 7676, copyHistory = null, true, true, true, true,
+            true, true, null, 2323
+        )
+        val post2 = Post(
+            0, 1, 2, 3, Date(), "здесь тектс поста", 4, 5,
+            true, comments, null, null, null, null, "post", null,
+            null, 7676, copyHistory = null, true, true, true, true,
+            true, true, null, 2323
+        )
+        WallService.add(post1)
+        WallService.add(post2)
+        val thread = Thread(999, true, true, true)
+        val comment = Comment(
+            666, 2, 23000, 111, "тут текст комментария", null, null,
+            null, null, thread
+        )
+        WallService.createComment(comment)
+        val reportComment = ReportComment(222, 666, 9)
+        WallService.createReportComment(reportComment)
+    }
+
+    @Test
+    fun countPosts() {
+        val comments = Comments(1, canPost = true, groupsCanPost = true, canClose = true, canOpen = true)
+        val post1 = Post(
+            0, 1, 2, 3, Date(), "здесь тектс поста", 4, 5,
+            true, comments, null, null, null, null, "post", null,
+            null, 7676, copyHistory = null, true, true, true, true,
+            true, true, null, 2323
+        )
+        val post2 = Post(
+            0, 1, 2, 3, Date(), "здесь тектс поста", 4, 5,
+            true, comments, null, null, null, null, "post", null,
+            null, 7676, copyHistory = null, true, true, true, true,
+            true, true, null, 2323
+        )
+        WallService.add(post1)
+        WallService.add(post2)
+        val expected = 2
+        val actual = WallService.countPosts()
+        assertEquals(expected, actual)
     }
 
 }
